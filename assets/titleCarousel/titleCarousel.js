69d526e7dfbf43cc4a45113850b68d74cf7bc730
Jose Angel Roa Gomez <jangelroa@gmail.com>
2018-09-23 08:56:42 -0700

    wip

---
+++ assets/titleCarousel/titleCarousel.js
@@ -0,0 +1,59 @@
+let titleCarouselWrapper = () => {
+  let titleCarousel = document.getElementById('title-carousel');
+
+
+  let elementBuilder = (elementType, elementId, appendTo, classes) => {
+    let el = document.createElement(elementType);
+    if (classes) {
+      el.classList.add(...classes);
+    }
+    el.id = elementId;
+    appendTo.appendChild(el);
+  };
+
+
+
+  elementBuilder('p', 'left-arrow', titleCarousel, ['arrow', 'fas', 'fa-chevron-left']);
+  elementBuilder('p', 'title', titleCarousel);
+  elementBuilder('p', 'right-arrow', titleCarousel, ['arrow', 'fas', 'fa-chevron-right']);
+
+
+
+
+
+
+
+  let title = document.getElementById('title');
+  let titleSetter = (newTitle) => {
+    title.innerHTML = newTitle;
+  };
+
+  titleSetter('September');
+  let titleArr = JSON.parse(titleCarousel.getAttribute('title-arr'));
+  let titleIndex = Number(titleCarousel.getAttribute('title-starting index'));
+
+
+
+console.log(titleIndex)
+
+
+
+
+  let leftArrow = document.getElementById('left-arrow');
+  let rightArrow = document.getElementById('right-arrow');
+
+  leftArrow.addEventListener('click', ()=>{
+    if(titleIndex > 0){
+      titleIndex--;
+      titleSetter(titleArr[titleIndex]);
+    }
+  });
+  rightArrow.addEventListener('click', ()=>{
+    if(titleIndex < titleArr.length-1){
+      titleIndex++;
+      titleSetter(titleArr[titleIndex]);
+    }
+  });
+};
+
+document.addEventListener('DOMContentLoaded', titleCarouselWrapper, false);
